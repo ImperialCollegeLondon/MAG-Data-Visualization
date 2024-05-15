57a14c1ad9c8db217a84cc81e946558a649ebf02
mfacchinelli <m.facchinelli@imperial.ac.uk>
2024-05-15 13:04:28 +0100

    `mag.graphics.chart.Spectrogram` does not compute spectrogram, it just plots it

--- resources/release-notes.md
+++ resources/release-notes.md
@@ -1,17 +1,26 @@
 # Software
 
-- Add `mag.Spectrum` data class to capture spectrogram results
-- Add `mag.transform.Spectrogram` for computing spectrograms of science data
+## Graphics
+
 - Add separate figure in `mag.graphics.view.Frequency` to show field and spectrogram
-- Add `mag.time.Constant.Eps` for consistent definition of `eps` in seconds
 - Add `FrequencyPoints` option to specify number of frequency steps in `mag.graphics.chart.Spectrogram`
+- Do not use events for range event plotting in `mag.graphics.view.Field`
+- Do not reshape compute PSD in `mag.graphics.view.PSD`, if sizes do not match
+
+## Spectrogram
+
+- Add `mag.Spectrum` data class to capture spectrogram results
+- Add `mag.transform.Spectrogram` for computing spectrograms for all different time and mode periods separately
+- Rename `mag.computeSpectrogram` to `mag.spectrogram`
+- Update `mag.spectrogram` as a wrapper of `mag.transform.Spectrogram`
+- `mag.graphics.chart.Spectrogram` does not compute spectrogram, it just plots it
+
+## Other
+
+- Add `mag.time.Constant.Eps` for consistent definition of `eps` in seconds
 - Improve algorithm for correction of mode change event timestamp (only correct mode changes and increase search window)
 - Detect mode changes from timestamp cadence, when no event data is available
 - Improve algorithm for detecting mode and range cycling
-- Rename `mag.computeSpectrogram` to `mag.spectrogram`
-- Update `mag.spectrogram` to compute spectrogram for all different time periods separately
 - Allow cropping with `datetime` pair denoting start and end times
 - Do not improve event time estimates for Config mode
 - Do not crop events if endpoints do not include any data
-- Do not use events for range event plotting in `mag.graphics.view.Field`
-- Do not reshape compute PSD in `mag.graphics.view.PSD`, if sizes do not match
--- src/visualize/+mag/+graphics/+chart/Spectrogram.m
+++ src/visualize/+mag/+graphics/+chart/Spectrogram.m
@@ -1,20 +1,6 @@
 classdef Spectrogram < mag.graphics.chart.Chart
 % SPECTROGRAM Definition of chart of "spectrogram" type.
 
-    properties
-        % NORMALIZE Normalize data before computing spectrum to highlight
-        % spikes.
-        Normalize (1, 1) logical = true
-        % FREQUENCYLIMITS Specifies the frequency band limits.
-        FrequencyLimits (1, 2) double = [missing(), missing()]
-        % FREQUENCYPOINTS Number of frequency samples.
-        FrequencyPoints (1, 1) double = 256
-        % WINDOW Length of window.
-        Window (1, 1) double = missing()
-        % OVERLAP Number of overlapped samples.
-        Overlap (1, 1) double = missing()
-    end
-
     methods
 
         function this = Spectrogram(options)
@@ -30,7 +16,7 @@ classdef Spectrogram < mag.graphics.chart.Chart
 
             arguments (Input)
                 this (1, 1) mag.graphics.chart.Spectrogram
-                data {mustBeA(data, ["mag.Data", "timetable"])}
+                data (1, :) mag.Spectrum
                 axes (1, 1) matlab.graphics.axis.Axes
                 ~
             end
@@ -39,29 +25,19 @@ classdef Spectrogram < mag.graphics.chart.Chart
                 graph (1, :) matlab.graphics.Graphics
             end
 
-            xData = this.getXData(data);
-            yData = this.getYData(data);
+            hold(axes, "on");
+            resetAxesHold = onCleanup(@() hold(axes, "off"));
 
-            % Scale overlap parameter based on number of elements.
-            N = numel(xData);
-
-            if ~ismissing(this.Overlap)
-                overlap = this.Overlap;
-            elseif N < 1e4
-                overlap = 0.8;
-            elseif N > 1e6
-                overlap = 0.3;
-            else
-                overlap = 0.8 + (N * (0.8 - 0.3) / (1e4 - 1e6));
-            end
-
-            % Spectrogram.
-            [f, t, p] = mag.spectrogram(xData, yData, FrequencyLimits = this.FrequencyLimits, FrequencyPoints = this.FrequencyPoints, ...
-                Normalize = this.Normalize, Window = this.Window, Overlap = overlap);
+            graph = matlab.graphics.chart.primitive.Surface.empty();
 
             % Plot.
-            graph = surf(axes, t, f, pow2db(abs(p)), EdgeColor = "none");
+            for d = data
 
+                p = this.YVariables.applyAll(d);
+                graph(end + 1) = surf(axes, d.Time, d.Frequency, pow2db(abs(p)), EdgeColor = "none"); %#ok<AGROW>
+            end
+
+            % Arrange.
             axes.XLimitMethod = "tight";
             axes.YLimitMethod = "tight";
 
