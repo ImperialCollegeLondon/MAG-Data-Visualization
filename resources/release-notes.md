c061541ed4056994b673e9f2edac038cc1a33a46
mfacchinelli <m.facchinelli@imperial.ac.uk>
2024-05-24 11:40:06 +0100

    fix: make loading of CDF sensors more flexible to other CDF files

--- resources/release-notes.md
+++ resources/release-notes.md
@@ -1,4 +1,5 @@
 # Software
 
+- Make loading of CDF sensors more flexible to other CDF files
 - Do not use cropped analysis for `mag.graphics.views.Timestamp` plot in `mag.graphics.sftPlots`
 - Fix issue with I-ALiRT primary and secondary having different length after ramp mode crop
--- src/io/+mag/+io/+in/CDF.m
+++ src/io/+mag/+io/+in/CDF.m
@@ -5,14 +5,20 @@ classdef (Abstract) CDF < mag.io.in.Format
         Extension = ".cdf"
     end
 
+    properties
+        % KEEPEPOCHASIS Boolean denoting whether to keep CDF epoch data
+        % values as is.
+        KeepEpochAsIs (1, 1) logical = true
+    end
+
     methods
 
-        function [rawData, cdfInfo] = load(~, fileName)
+        function [rawData, cdfInfo] = load(this, fileName)
 
             assert(exist("spdfcdfinfo", "file"), "SPDF CDF Toolbox needs to be installed.");
 
             cdfInfo = spdfcdfinfo(fileName);
-            rawData = spdfcdfread(fileName, 'KeepEpochAsIs', true);
+            rawData = spdfcdfread(fileName, 'KeepEpochAsIs', this.KeepEpochAsIs);
         end
     end
 end
