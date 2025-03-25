f731246bb5222629f6ef1b14a7fc85b8741990e0
mfacchinelli <m.facchinelli@imperial.ac.uk>
2025-03-25 19:50:05 +0000

    test: add more tests for app, including debug mode

--- tests/system/app/tDataVisualization.m
+++ tests/system/app/tDataVisualization.m
@@ -4,10 +4,12 @@ classdef tDataVisualization < AppTestCase
     properties (TestParameter)
         ValidMission = {"Bartington", "HelioSwarm", "IMAP"}
         InvalidMission = {"SolarOrbiter", "Not a Mission"}
+        BreakpointType = {mag.app.manage.ToolbarManager.DebugErrorID, mag.app.manage.ToolbarManager.DebugErrorSource}
     end
 
     methods (Test)
 
+        % Test that app can be opened for all supported missions.
         function startApp_validMission(testCase, ValidMission)
 
             % Exercise.
@@ -20,8 +22,63 @@ classdef tDataVisualization < AppTestCase
             testCase.verifyAppUIElementStatus(app, "off");
         end
 
+        % Test that error is thrown for unsupported missions.
         function startApp_invalidMission(testCase, InvalidMission)
             testCase.verifyError(@() DataVisualization(InvalidMission), ?MException, "Error should be thrown on invalid mission.");
+        end
+
+        % Test that analyzing empty folder issues a warning.
+        function analyze_noData(testCase)
+
+            % Set up.
+            workingDirectory = testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture());
+
+            app = testCase.createAppWithCleanup("IMAP");
+
+            % Exercise.
+            testCase.type(app.AnalysisManager.LocationEditField, workingDirectory.Folder);
+            testCase.press(app.ProcessDataButton);
+
+            % Verify.
+            testCase.dismissDialog("uialert", app.UIFigure);
+        end
+
+        % Test that debugging can be enabled.
+        function toolbarDebug(testCase, BreakpointType)
+
+            % Set up.
+            testCase.restoreDebugStatePostTest();
+
+            app = testCase.createAppWithCleanup("IMAP");
+
+            testCase.press(app.ProcessDataButton);
+            testCase.dismissDialog("uialert", app.UIFigure);
+
+            % Exercise.
+            testCase.chooseDialog("uiconfirm", app.UIFigure, @() testCase.press(app.ToolbarManager.DebugToggleTool), BreakpointType);
+
+            % Verify.
+            testCase.verifyNotEmpty(dbstatus(), "Breakpoint should be set.");
+
+            % Clean up.
+            testCase.press(app.ToolbarManager.DebugToggleTool);
+            testCase.verifyEmpty(dbstatus(), "Breakpoint should be reset.");
+        end
+
+        % Test that alert is shown when enabling debugging with no error.
+        function toolbarDebug_noError(testCase)
+
+            % Set up.
+            testCase.restoreDebugStatePostTest();
+
+            app = testCase.createAppWithCleanup("IMAP");
+
+            % Exercise.
+            testCase.press(app.ToolbarManager.DebugToggleTool);
+
+            % Verify.
+            testCase.dismissDialog("uialert", app.UIFigure);
+            testCase.verifyEmpty(dbstatus(), "No breakpoint should be set.");
         end
 
         % function selectMission(testCase, ValidMission)
@@ -40,5 +97,17 @@ classdef tDataVisualization < AppTestCase
         %     % Verify.
         %     testCase.verifyEqual(app.Mission, ValidMission, "Mission should be selected.");
         % end
+    end
+
+    methods (Access = private)
+
+        function restoreDebugStatePostTest(testCase)
+
+            status = dbstatus();
+            testCase.addTeardown(@() dbstop(status));
+
+            dbclear("all");
+            testCase.assertEmpty(dbstatus(), "Debugging status should be empty before test.");
+        end
     end
 end
