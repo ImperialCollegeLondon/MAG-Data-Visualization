ec3df6ad281d3e3d65c065ccf66f473dcf0ffe19
mfacchinelli <m.facchinelli@imperial.ac.uk>
2025-08-22 11:04:30 +0100

    style: allow class name starting with `t` for test cases

--- resources/release-notes.md
+++ resources/release-notes.md
@@ -1,3 +1,7 @@
 ## Software
 
 - (All) Add chart for plotting shaded regions (`mag.graphics.chart.Region`)
+
+## Package
+
+- Update code analyzer configuration to 1.2.0 (allow class name starting with `t` for test cases)
---
+++ tests/system/package/tCodeAnalyzerConfiguration.m
@@ -0,0 +1,23 @@
+classdef tCodeAnalyzerConfiguration < matlab.unittest.TestCase
+% TCODEANALYZERCONFIGURATION Tests for code analyzer configuration JSON
+% file.
+
+    properties (Constant, Access = public)
+        ConfigurationFile = fullfile(fileparts(mfilename("fullpath")), "..", "..", "..", "resources", "codeAnalyzerConfiguration.json")
+    end
+
+    methods (Test)
+
+        function validConfiguration(testCase)
+
+            % Set up.
+            testCase.assertThat(testCase.ConfigurationFile, matlab.unittest.constraints.IsFile(), "Configuration file should exist.");
+
+            % Exercise.
+            results = matlab.codeanalysis.validateConfiguration(testCase.ConfigurationFile);
+
+            % Verify.
+            testCase.verifyEmpty(results, "Configuration file should be valid.");
+        end
+    end
+end
