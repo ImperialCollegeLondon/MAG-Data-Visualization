741bb53b7b293c8804b26df7817cf83d2cc033be
mfacchinelli <m.facchinelli@imperial.ac.uk>
2025-09-15 15:11:16 +0100

    fix: enum variables cannot be converted from `missing`

--- src/utility/+mag/+internal/isMissingCompatible.m
+++ src/utility/+mag/+internal/isMissingCompatible.m
@@ -2,5 +2,5 @@ function tf = isMissingCompatible(x)
 % ISMISSINGCOMPATIBLE Determine whether input object is compatible with
 % "missing" type.
 
-    tf = isa(x, "single") | isa(x, "double") | isa(x, "duration") | isa(x, "calendarDuration") | isa(x, "datetime") | isa(x, "categorical") | isa(x, "string");
+    tf = (isa(x, "single") | isa(x, "double") | isa(x, "duration") | isa(x, "calendarDuration") | isa(x, "datetime") | isa(x, "categorical") | isa(x, "string")) & ~isenum(x);
 end
