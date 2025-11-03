ed3e4902aa5c26de07410ad00fadac608a5a9bfd
mfacchinelli <m.facchinelli@imperial.ac.uk>
2025-11-03 15:53:07 +0000

    fix: clearer help for piecewise noise function

--- src/mission/hs/+mag/+hs/+view/piecewiseNoiseThreshold.m
+++ src/mission/hs/+mag/+hs/+view/piecewiseNoiseThreshold.m
@@ -1,5 +1,6 @@
 function y = piecewiseNoiseThreshold(x)
-% PIECEWISENOISETHRESHOLD Generate noise as a piecewise function.
+% PIECEWISENOISETHRESHOLD Calculate noise threshold as a piecewise function
+% of frequency.
 
     y(x <= 1) = 1500;
     y((x > 1) & (x <= 2)) = 15;
